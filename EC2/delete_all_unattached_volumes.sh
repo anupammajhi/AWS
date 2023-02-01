fe8d0e6deebc6f2aa4123367a8898a1df1eab13d
Anupam Majhi <majhi.anupam@gmail.com>
2023-02-02 05:21:40 +0530

    Need to validate

--- EC2/delete_all_unattached_volumes.sh
+++ EC2/delete_all_unattached_volumes.sh
@@ -10,17 +10,3 @@ for region in $(aws ec2 describe-regions --output text --query 'Regions[*].Regio
   aws ec2 describe-volumes --region $region --output text --query 'Volumes[*].{Attachments:Attachments,VolumeId:VolumeId}' | \
   while read -r attachments volume_id; do
     if [[ -z "$attachments" ]]; then
-      echo "Deleting unattached volume $volume_id in region $region..."
-      aws ec2 delete-volume --volume-id $volume_id --region $region
-      count=$((count+1))
-    else
-      echo "Volume $volume_id is attached in region $region, skipping."
-    fi
-  done
-done
-
-if [[ $count -gt 0 ]]; then
-  echo "Deleted $count unattached volumes"
-else
-  echo "No unattached volumes found"
-fi
