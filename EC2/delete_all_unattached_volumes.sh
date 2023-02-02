3a874f5fcf14aef719fc43f93c177f70e47ea673
Anupam Majhi <majhi.anupam@gmail.com>
2023-02-02 06:19:35 +0530

    Enhance script

--- EC2/delete_all_unattached_volumes.sh
+++ EC2/delete_all_unattached_volumes.sh
@@ -5,3 +5,13 @@
 count=0
 
 for region in $(aws ec2 describe-regions --output text --query 'Regions[*].RegionName'); do
+  echo "Processing region $region..."
+
+  aws ec2 describe-volumes --region $region --output text --query 'Volumes[*].{Attachments:Attachments,VolumeId:VolumeId}' | \
+  while read -r attachments volume_id; do
+    if [[ -z "$attachments" ]]; then
+      echo "Deleting unattached volume $volume_id in region $region..."
+      aws ec2 delete-volume --volume-id $volume_id --region $region
+      count=$((count+1))
+    else
+      echo "Volume $volume_id is attached in region $region, skipping."
