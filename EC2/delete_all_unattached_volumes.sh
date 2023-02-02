cf1a46ee61fa9a27360db5979cd5964fc421b9ef
Anupam Majhi <majhi.anupam@gmail.com>
2023-02-02 05:59:15 +0530

    Refactor code for better code readability

--- EC2/delete_all_unattached_volumes.sh
+++ EC2/delete_all_unattached_volumes.sh
@@ -5,6 +5,3 @@
 count=0
 
 for region in $(aws ec2 describe-regions --output text --query 'Regions[*].RegionName'); do
-  echo "Processing region $region..."
-
-  aws ec2 describe-volumes --region $region --output text --query 'Volumes[*].{Attachments:Attachments,VolumeId:VolumeId}' | \
