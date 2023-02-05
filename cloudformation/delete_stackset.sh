a92732c0f21c0d2390d883e7b481965b5844759a
Anupam Majhi <majhi.anupam@gmail.com>
2023-02-05 07:24:37 +0530

    Changes in code

--- cloudformation/delete_stackset.sh
+++ cloudformation/delete_stackset.sh
@@ -5,3 +5,9 @@
 region="us-west-2"
 retain_stacks=true
 
+help_document() {
+    printf "Usage: %s <stackset_name>\n" "$0"
+    exit 0
+}
+
+if [[ $1 == "help" || $1 == "h" || $1 == "--help" ]]; then
