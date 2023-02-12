415e9ebb24ded6c9a39f13ef400216f7fec6d7a8
Anupam Majhi <majhi.anupam@gmail.com>
2023-02-12 05:58:58 +0530

    Update

--- cloudwatch/set_cloudwatch_logs_retention.sh
+++ cloudwatch/set_cloudwatch_logs_retention.sh
@@ -6,3 +6,9 @@ if [[ -z $1 ]]; then
     echo "Error: No argument provided. Usage: ./script.sh <retention>"
     exit 1
 fi
+
+allowed_values=(1 3 5 7 14 30 60 90 120 150 180 365 400 545 731 1827 3653)
+if [[ ! "${allowed_values[@]}" =~ (^|[[:space:]])"$1"($|[[:space:]]) ]]; then
+    echo "Invalid retention value. Please choose one of the allowed values: ${allowed_values[@]}"
+    exit 1
+fi
