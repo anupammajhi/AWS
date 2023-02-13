2ebb7f2643e34784287d5f3bbdf1ab14214306f1
Anupam Majhi <majhi.anupam@gmail.com>
2023-02-13 08:05:18 +0530

    Code change

--- cloudwatch/set_cloudwatch_logs_retention.sh
+++ cloudwatch/set_cloudwatch_logs_retention.sh
@@ -19,3 +19,9 @@ if [[ $1 == "help" || $1 == "h" || $1 == "--help" ]]; then
     echo "Possible retention values: 1, 3, 5, 7, 14, 30, 60, 90, 120, 150, 180, 365, 400, 545, 731, 1827, 3653"
     exit 0
 fi
+
+retention=$1
+echo "Setting CloudWatch Logs Retention Policy to $retention days for all log groups in the region."
+
+# Check if the log group exists before setting the retention policy
+if aws logs describe-log-groups --log-group-name /aws/lambda/* --query 'logGroups[].logGroupName' --output text | grep -q '/aws/lambda/'; then
