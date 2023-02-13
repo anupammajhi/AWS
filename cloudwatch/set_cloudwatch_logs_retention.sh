55a97da47b9debb5232b864759ace988216ee5e1
Anupam Majhi <majhi.anupam@gmail.com>
2023-02-13 07:25:00 +0530

    Quick Commit

--- cloudwatch/set_cloudwatch_logs_retention.sh
+++ cloudwatch/set_cloudwatch_logs_retention.sh
@@ -13,3 +13,8 @@ if [[ ! "${allowed_values[@]}" =~ (^|[[:space:]])"$1"($|[[:space:]]) ]]; then
     exit 1
 fi
 
+if [[ $1 == "help" || $1 == "h" || $1 == "--help" ]]; then
+    echo "Usage: ./script.sh <retention>"
+    echo "Set a retention in days for all your CloudWatch Logs in a single region."
+    echo "Possible retention values: 1, 3, 5, 7, 14, 30, 60, 90, 120, 150, 180, 365, 400, 545, 731, 1827, 3653"
+    exit 0
