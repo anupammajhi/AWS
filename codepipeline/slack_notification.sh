1d45f8b9c1d9a9bfb174fba009be1ba60654fbf2
Anupam Majhi <majhi.anupam@gmail.com>
2023-02-17 07:11:40 +0530

    Update

---
+++ codepipeline/slack_notification.sh
@@ -0,0 +1,27 @@
+﻿
+#!/bin/bash
+## Author: Anupam Majhi
+## Github: https://github.com/anupammajhi/AWS
+
+if [ "$1" == "help" ] || [ "$1" == "h" ] || [ "$1" == "--help" ]; then
+    echo "Usage: bash script.sh [options]"
+    exit 0
+fi
+
+ACCOUNT_DESC="$ACCOUNT_DESC" 
+SLACK_URL="$SLACK_URL" 
+SLACK_CHANNEL="$SLACK_CHANNEL" 
+ALWAYS_SHOW_SUCCEEDED="$ALWAYS_SHOW_SUCCEEDED"
+
+get_previous_pipeline_execution() {
+    pipeline_executions=$(aws codepipeline list-pipeline-executions --pipeline-name $1 | jq '.pipelineExecutionSummaries')
+
+    is_next=false
+
+    for item in $pipeline_executions; do
+        status=$(echo $item | jq -r '.status')
+        if $is_next && ([ "$status" == "Succeeded" ] || [ "$status" == "Failed" ]); then
+            echo $item
+            return
+        fi
+        pipelineExecutionId=$(echo $item | jq -r '.pipelineExecutionId')
