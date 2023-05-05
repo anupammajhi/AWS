50ff310c4601d505d707921f64fef9c4febeec78
Anupam Majhi <majhi.anupam@gmail.com>
2023-05-05 08:20:00 +0530

    Update and commit

---
+++ ecs/delete_all_inactive_task_definitions.sh
@@ -0,0 +1,14 @@
+﻿
+#!/bin/bash
+## Author: Anupam Majhi
+## Github: https://github.com/anupammajhi/AWS
+
+help_document() {
+    echo "Usage: $0 [region]"
+    echo "If region is not provided, it will delete inactive task definitions in all AWS regions."
+    exit 1
+}
+
+if [[ $1 == "help" || $1 == "h" || $1 == "--help" ]]; then
+    help_document
+fi
