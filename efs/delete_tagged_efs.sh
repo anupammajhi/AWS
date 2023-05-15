888748b4fbe899c9ca1e24b8c1a6c68f093fa67b
Anupam Majhi <majhi.anupam@gmail.com>
2023-05-15 05:36:40 +0530

    Update and commit

---
+++ efs/delete_tagged_efs.sh
@@ -0,0 +1,9 @@
+﻿#!/bin/bash
+## Author: Anupam Majhi
+## Github: https://github.com/anupammajhi/AWS
+
+if [ "$1" == "help" ] || [ "$1" == "h" ] || [ "$1" == "--help" ]; then
+    echo "This script finds and deletes all tagged elastic file systems including mount targets"
+    exit 0
+fi
+
