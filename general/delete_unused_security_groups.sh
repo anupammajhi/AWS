6b624ca6f15dfa12f31ec5178992db5612cdd5c2
Anupam Majhi <majhi.anupam@gmail.com>
2023-05-17 07:05:00 +0530

    Change code

---
+++ general/delete_unused_security_groups.sh
@@ -0,0 +1,15 @@
+﻿
+#!/bin/bash
+## Author: Anupam Majhi
+## Github: https://github.com/anupammajhi/AWS
+
+if [[ $1 == "help" || $1 == "h" || $1 == "--help" ]]; then
+    echo "Usage: $0"
+    echo "This script deletes all unused security groups in a single AWS Region"
+    exit
+fi
+
+aws_profile="default"
+region="us-west-1"
+
+used_SG=()
