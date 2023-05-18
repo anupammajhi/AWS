b31dfe59d26d8841707fb6e2a7b0f0d320245448
Anupam Majhi <majhi.anupam@gmail.com>
2023-05-18 07:53:20 +0530

    Change code

--- general/delete_unused_security_groups.sh
+++ general/delete_unused_security_groups.sh
@@ -6,3 +6,19 @@
 if [[ $1 == "help" || $1 == "h" || $1 == "--help" ]]; then
     echo "Usage: $0"
     echo "This script deletes all unused security groups in a single AWS Region"
+    exit
+fi
+
+aws_profile="default"
+region="us-west-1"
+
+used_SG=()
+
+# Find EC2 instances security group in use
+response=$(aws ec2 describe-instances --profile $aws_profile --region $region)
+for sg in $(echo $response | jq -r '.Reservations[].Instances[].SecurityGroups[].GroupId'); do
+    used_SG+=($sg)
+done
+
+# Find Classic load balancer security group in use
+response=$(aws elb describe-load-balancers --profile $aws_profile --region $region)
