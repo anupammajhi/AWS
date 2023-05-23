44e10a0683b9eb8050437cd8c8cf167dad8e27ad
Anupam Majhi <majhi.anupam@gmail.com>
2023-05-23 06:18:20 +0530

    Change code

---
+++ general/find_unused_security_groups.sh
@@ -0,0 +1,11 @@
+﻿
+if [ "$1" == "help" ] || [ "$1" == "h" ] || [ "$1" == "--help" ]; then
+    echo "Usage: $0"
+    echo "This script finds all unused security groups in a single AWS Region"
+    exit
+fi
+
+ec2=$(aws ec2 describe-instances)
+elb=$(aws elb describe-load-balancers)
+elbv2=$(aws elbv2 describe-load-balancers)
+rds=$(aws rds describe-db-instances)
