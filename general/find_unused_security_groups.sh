b6ea134545b196af7ab1d7423856285f4eaf255b
Anupam Majhi <majhi.anupam@gmail.com>
2023-05-23 06:37:26 +0530

    Update and commit

--- general/find_unused_security_groups.sh
+++ general/find_unused_security_groups.sh
@@ -5,7 +5,3 @@ if [ "$1" == "help" ] || [ "$1" == "h" ] || [ "$1" == "--help" ]; then
     exit
 fi
 
-ec2=$(aws ec2 describe-instances)
-elb=$(aws elb describe-load-balancers)
-elbv2=$(aws elbv2 describe-load-balancers)
-rds=$(aws rds describe-db-instances)
