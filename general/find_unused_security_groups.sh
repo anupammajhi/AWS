2c7bc8ac93952bb0b18028cde8ae36a2691a81fb
Anupam Majhi <majhi.anupam@gmail.com>
2023-05-24 05:18:20 +0530

    Quick Commit

--- general/find_unused_security_groups.sh
+++ general/find_unused_security_groups.sh
@@ -5,3 +5,16 @@ if [ "$1" == "help" ] || [ "$1" == "h" ] || [ "$1" == "--help" ]; then
     exit
 fi
 
+ec2=$(aws ec2 describe-instances)
+elb=$(aws elb describe-load-balancers)
+elbv2=$(aws elbv2 describe-load-balancers)
+rds=$(aws rds describe-db-instances)
+
+used_SG=()
+
+for instance in $(echo "$ec2" | jq -r '.Reservations[].Instances[]'); do
+    for sg in $(echo "$instance" | jq -r '.SecurityGroups[].GroupId'); do
+        used_SG+=("$sg")
+    done
+done
+
