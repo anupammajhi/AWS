50b977debab9114e904c16734c98453ac1e7d015
Anupam Majhi <majhi.anupam@gmail.com>
2023-05-28 06:31:28 +0530

    Code change

--- general/multi_account_execution.sh
+++ general/multi_account_execution.sh
@@ -4,3 +4,11 @@
 ## Github: https://github.com/anupammajhi/AWS
 
 if [ "$1" == "help" ] || [ "$1" == "h" ] || [ "$1" == "--help" ]; then
+    echo "Usage: bash script.sh"
+    exit 0
+fi
+
+role_arn_to_session() {
+    client=$(aws sts assume-role "$@")
+    access_key_id=$(echo "$client" | jq -r '.Credentials.AccessKeyId')
+    secret_access_key=$(echo "$client" | jq -r '.Credentials.SecretAccessKey')
