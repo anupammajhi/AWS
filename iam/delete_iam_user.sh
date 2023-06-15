87f89b43224ad21fd319dfb2e5c2fa5a8f13dd16
Anupam Majhi <majhi.anupam@gmail.com>
2023-06-15 06:20:52 +0530

    Update

--- iam/delete_iam_user.sh
+++ iam/delete_iam_user.sh
@@ -7,3 +7,11 @@ if [ "$1" == "help" ] || [ "$1" == "h" ] || [ "$1" == "--help" ]; then
     echo "Usage: $0 [username]"
     exit 1
 fi
+
+username=$1
+
+delete_access_keys() {
+    response=$(aws iam list-access-keys --user-name $username)
+    for access_key in $(echo $response | jq -r ".AccessKeyMetadata[] | .AccessKeyId"); do
+        aws iam delete-access-key --user-name $username --access-key-id $access_key
+    done
