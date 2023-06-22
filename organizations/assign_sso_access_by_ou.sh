1d7f87f5f91b04619da3e367fdbb7ae111418362
Anupam Majhi <majhi.anupam@gmail.com>
2023-06-22 06:55:00 +0530

    Commit to change

---
+++ organizations/assign_sso_access_by_ou.sh
@@ -0,0 +1,41 @@
+﻿#!/bin/bash
+## Author: Anupam Majhi
+## Github: https://github.com/anupammajhi/AWS
+
+if [[ $1 == "help" || $1 == "h" || $1 == "--help" ]]; then
+    echo "This script assigns AWS Single Sign-On (SSO) access to a specified principal (user or group) for multiple AWS accounts within a specified Organizational Unit (OU)."
+    echo "Usage: ./script.sh"
+    exit
+fi
+
+# Replace these variables with your values
+PRINCIPAL_NAME="Administrators"  # e.g., 'user_name' or 'group_name'
+PRINCIPAL_TYPE="GROUP"  # e.g., 'USER' or 'GROUP'
+PERMISSION_SET_NAME="AdministratorAccess"
+OU_NAME="Sandbox"  # Replace with the OU name you want to fetch accounts from
+
+# Function to get instance information from AWS SSO
+get_instance_information() {
+    response=$(aws sso-admin list-instances)
+    if [[ -z $response ]]; then
+        echo "No SSO instances found"
+        exit 1
+    fi
+    instance_info=$(echo $response | jq -r '.Instances[0]')
+    echo $instance_info | jq -r '.InstanceArn, .IdentityStoreId'
+}
+
+# Function to get account IDs in an Organizational Unit (OU) given its name
+get_accounts_in_ou() {
+    root_id=$(aws organizations list-roots | jq -r '.Roots[0].Id')
+    ou_id=$(aws organizations list-organizational-units-for-parent --parent-id $root_id | jq -r --arg ou "$OU_NAME" '.OrganizationalUnits[] | select(.Name == $ou) | .Id')
+    if [[ -z $ou_id ]]; then
+        echo "Organizational Unit not found: $OU_NAME"
+        exit 1
+    fi
+    accounts=$(aws organizations list-accounts-for-parent --parent-id $ou_id | jq -r '.Accounts[].Id')
+    echo $accounts
+}
+
+# Get the principal ID for the specified principal name and type
+get_principal_id() {
