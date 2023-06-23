1d0df65d83cd93e86b3e04dc4c01a71373903472
Anupam Majhi <majhi.anupam@gmail.com>
2023-06-23 06:23:20 +0530

    Changes in code

--- organizations/assign_sso_access_by_ou.sh
+++ organizations/assign_sso_access_by_ou.sh
@@ -30,3 +30,44 @@ get_accounts_in_ou() {
     root_id=$(aws organizations list-roots | jq -r '.Roots[0].Id')
     ou_id=$(aws organizations list-organizational-units-for-parent --parent-id $root_id | jq -r --arg ou "$OU_NAME" '.OrganizationalUnits[] | select(.Name == $ou) | .Id')
     if [[ -z $ou_id ]]; then
+        echo "Organizational Unit not found: $OU_NAME"
+        exit 1
+    fi
+    accounts=$(aws organizations list-accounts-for-parent --parent-id $ou_id | jq -r '.Accounts[].Id')
+    echo $accounts
+}
+
+# Get the principal ID for the specified principal name and type
+get_principal_id() {
+    identity_store_id=$1
+    principal_name=$2
+    principal_type=$3
+    filter='{"AttributePath": "UserName" if principal_type == "USER" else "DisplayName","AttributeValue": principal_name}'
+    response=$(aws identitystore list-users --identity-store-id $identity_store_id --filters $filter)
+    if [[ ! $(echo $response | jq -r '.Users') && ! $(echo $response | jq -r '.Groups') ]]; then
+        echo "Principal not found: $principal_name"
+        exit 1
+    fi
+    principal_id=$(echo $response | jq -r --arg principal_type "$principal_type" '.[$principal_type + "s"][0]."${principal_type}Id"')
+    echo $principal_id
+}
+
+# Get the Permission Set ARN for the specified Permission Set name
+get_permission_set_arn() {
+    instance_arn=$1
+    permission_set_name=$2
+    response=$(aws sso-admin list-permission-sets --instance-arn $instance_arn)
+    for permission_set_arn in $(echo $response | jq -r '.PermissionSets[]'); do
+        permission=$(aws sso-admin describe-permission-set --instance-arn $instance_arn --permission-set-arn $permission_set_arn)
+        if [[ $(echo $permission | jq -r '.PermissionSet.Name') == $permission_set_name ]]; then
+            echo $permission_set_arn
+            return
+        fi
+    done
+    echo "Permission set not found: $permission_set_name"
+    exit 1
+}
+
+# Assign access to the principal for each account in the OU
+assign_access_to_principal() {
+    instance_arn=$1
