4089e4acf55f790db3b77ef1c9a21907643ace4a
Anupam Majhi <majhi.anupam@gmail.com>
2023-07-04 05:35:00 +0530

    Line update

---
+++ organizations/list_accounts_by_ou.sh
@@ -0,0 +1,14 @@
+﻿
+#!/bin/bash
+## Author: Anupam Majhi
+## Github: https://github.com/anupammajhi/AWS
+
+# Help document function
+if [[ $1 == "help" ]] || [[ $1 == "h" ]] || [[ $1 == "--help" ]]; then
+    echo "Usage: ./script.sh [list of OU names]"
+    echo "This script returns a list of accounts that are part of an Organizational Unit (OU)"
+    exit
+fi
+
+ou_names=("$@")  # Get the list of organizational unit names from command-line arguments
+
