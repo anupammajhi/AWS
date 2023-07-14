910e80c6bdb601a0f22797f9df7db49c1c0f4253
Anupam Majhi <majhi.anupam@gmail.com>
2023-07-14 08:28:04 +0530

    Update and commit

--- organizations/remove_sso_access_by_ou.sh
+++ organizations/remove_sso_access_by_ou.sh
@@ -1,2 +1,10 @@
 ﻿
 #!/bin/bash
+## Author: Anupam Majhi
+## Github: https://github.com/anupammajhi/AWS
+
+if [ "$1" == "help" ] || [ "$1" == "h" ] || [ "$1" == "--help" ]; then
+    echo "Usage: $0 [OU_NAME]"
+    exit
+fi
+
