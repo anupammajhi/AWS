68e13639ab5dcfd9aacedb4c64c5f596043b7546
Anupam Majhi <majhi.anupam@gmail.com>
2023-07-15 07:41:40 +0530

    Changes in code

--- organizations/remove_sso_access_by_ou.sh
+++ organizations/remove_sso_access_by_ou.sh
@@ -8,3 +8,15 @@ if [ "$1" == "help" ] || [ "$1" == "h" ] || [ "$1" == "--help" ]; then
     exit
 fi
 
+PRINCIPAL_NAME="Administrators"
+PRINCIPAL_TYPE="GROUP"
+PERMISSION_SET_NAME="AWSAdministratorAccess"
+OU_NAME="$1"
+
+get_instance_information() {
+    response=$(aws sso-admin list-instances)
+    instance_arn=$(echo $response | jq -r '.Instances[0].InstanceArn')
+    identity_store_id=$(echo $response | jq -r '.Instances[0].IdentityStoreId')
+    echo "$instance_arn $identity_store_id"
+}
+
