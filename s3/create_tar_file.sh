10ab2c4a299afda4190cc8b069e9263f110ab244
Anupam Majhi <majhi.anupam@gmail.com>
2023-07-16 06:50:29 +0530

    Quick Commit

--- s3/create_tar_file.sh
+++ s3/create_tar_file.sh
@@ -5,3 +5,9 @@
 
 help_document() {
     echo "This script allows you to create tar file creation."
+    echo "Reference question : https://stackoverflow.com/questions/64341192/how-to-create-a-tar-file-containing-all-the-files-in-a-directory/64341789#64341789"
+    exit 0
+}
+
+if [[ $1 == "help" || $1 == "h" || $1 == "--help" ]]; then
+    help_document
