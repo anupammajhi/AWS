4f17e66b9a6ee64ea1b76545897a68a16d6626e4
Anupam Majhi <majhi.anupam@gmail.com>
2023-07-21 05:57:32 +0530

    Change code

--- s3/delete_empty_buckets.sh
+++ s3/delete_empty_buckets.sh
@@ -6,3 +6,12 @@ if [ "$1" == "help" ] || [ "$1" == "h" ] || [ "$1" == "--help" ]; then
     echo "Usage: $0"
     echo "This script searches for empty S3 buckets without versioning enabled and deletes them."
     exit 0
+fi
+
+s3_client=$(aws s3api)
+s3=$(aws s3)
+
+response=$(s3_client list-buckets)
+buckets=$(echo $response | jq '.Buckets')
+
+empty_buckets=()
