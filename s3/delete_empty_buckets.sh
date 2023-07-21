2cca49974ef16b2ccebc4b21ac8a5311019ad377
Anupam Majhi <majhi.anupam@gmail.com>
2023-07-21 05:35:00 +0530

    Line update

---
+++ s3/delete_empty_buckets.sh
@@ -0,0 +1,8 @@
+﻿#!/bin/bash
+## Author: Anupam Majhi
+## Github: https://github.com/anupammajhi/AWS
+
+if [ "$1" == "help" ] || [ "$1" == "h" ] || [ "$1" == "--help" ]; then
+    echo "Usage: $0"
+    echo "This script searches for empty S3 buckets without versioning enabled and deletes them."
+    exit 0
