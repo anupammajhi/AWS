583ce3fa97761f5c87eeeccfaeefc2ad320ce011
Anupam Majhi <majhi.anupam@gmail.com>
2023-08-04 07:41:40 +0530

    Fix line

---
+++ s3/list_file_older_than_number_of_days.sh
@@ -0,0 +1,5 @@
+﻿
+if [ "$1" = "help" ] || [ "$1" = "h" ] || [ "$1" = "--help" ]; then
+    echo "This script allows you to list all files older than N numbers of days."
+    echo "Usage: bash script.sh [N]"
+    exit 0
