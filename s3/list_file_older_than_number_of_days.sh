82f1f7242102122fb43238240034b8bc472fc47d
Anupam Majhi <majhi.anupam@gmail.com>
2023-08-04 08:01:22 +0530

    Changes in code

--- s3/list_file_older_than_number_of_days.sh
+++ s3/list_file_older_than_number_of_days.sh
@@ -3,3 +3,9 @@ if [ "$1" = "help" ] || [ "$1" = "h" ] || [ "$1" = "--help" ]; then
     echo "This script allows you to list all files older than N numbers of days."
     echo "Usage: bash script.sh [N]"
     exit 0
+fi
+
+client='boto3.client("s3")'
+response='client.list_objects(Bucket="angularbuildbucket")'
+today_date_time='date +%Y-%m-%d'
+for file in response.get("Contents"):
