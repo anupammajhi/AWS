e0f650d0689dae893bd8fb14dfbaf2002248f2bb
Anupam Majhi <majhi.anupam@gmail.com>
2023-08-10 05:21:40 +0530

    Fix line

---
+++ s3/search_bucket_and_delete.sh
@@ -0,0 +1,9 @@
+﻿#!/bin/bash
+## Author: Anupam Majhi
+## Github: https://github.com/anupammajhi/AWS
+
+# Help document function
+help_function() {
+    echo "Usage: $0 <bucket-name>"
+    exit 0
+}
