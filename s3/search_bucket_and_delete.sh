6f1b25bd9629c711f0ac8209ad3a94a3a17fa731
Anupam Majhi <majhi.anupam@gmail.com>
2023-08-10 06:04:23 +0530

    Quick Fix

--- s3/search_bucket_and_delete.sh
+++ s3/search_bucket_and_delete.sh
@@ -18,3 +18,15 @@ if [ -z "$1" ]; then
 fi
 
 target_bucket_name=$1
+
+s3_bucket_list=$(aws s3api list-buckets --query "Buckets[].Name" --output text)
+
+for bucket_name in $s3_bucket_list; do
+    if [[ $bucket_name == *"$target_bucket_name"* ]]; then
+        echo "Found bucket: $bucket_name"
+
+        versioning_status=$(aws s3api get-bucket-versioning --bucket $bucket_name --query "Status" --output text)
+
+        if [ "$versioning_status" == "Enabled" ]; then
+            aws s3 rm s3://$bucket_name --recursive --version-id-marker null
+        else
