e3ae66b91d988d9e8004e75ff926b6f6b456eda9
Anupam Majhi <majhi.anupam@gmail.com>
2023-08-10 05:49:39 +0530

    Quick Commit

--- s3/search_bucket_and_delete.sh
+++ s3/search_bucket_and_delete.sh
@@ -8,3 +8,13 @@ help_function() {
     exit 0
 }
 
+if [ "$1" == "help" ] || [ "$1" == "h" ] || [ "$1" == "--help" ]; then
+    help_function
+fi
+
+if [ -z "$1" ]; then
+    echo "Please provide the target bucket name as a command line argument"
+    exit 1
+fi
+
+target_bucket_name=$1
