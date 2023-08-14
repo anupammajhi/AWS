c9efc0f4dd844f8adddeeac06115c0f3c50fdb8d
Anupam Majhi <majhi.anupam@gmail.com>
2023-08-14 05:44:42 +0530

    Fix line

--- s3/search_file_in_bucket.sh
+++ s3/search_file_in_bucket.sh
@@ -7,3 +7,4 @@ s3=boto3.client('s3')
 def ListFiles(client, bucket_name, prefix):
     _BUCKET_NAME=bucket_name
     _PREFIX=prefix
+    response=client.list_objects(Bucket=_BUCKET_NAME, Prefix=_PREFIX)
