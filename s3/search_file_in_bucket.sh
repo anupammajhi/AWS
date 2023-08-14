b14b18cca51613acb3e910786ea0aa22b2efd4e5
Anupam Majhi <majhi.anupam@gmail.com>
2023-08-14 06:02:23 +0530

    Code change

--- s3/search_file_in_bucket.sh
+++ s3/search_file_in_bucket.sh
@@ -8,3 +8,8 @@ def ListFiles(client, bucket_name, prefix):
     _BUCKET_NAME=bucket_name
     _PREFIX=prefix
     response=client.list_objects(Bucket=_BUCKET_NAME, Prefix=_PREFIX)
+    for content in response.get('Contents', []):
+        yield content.get('Key')
+
+result=client.list_objects(Bucket=bucket_name, Delimiter='/')
+for obj in result.get('CommonPrefixes'):
