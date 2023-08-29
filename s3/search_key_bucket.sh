939f1739383c8b2b9b51bd1a7e527c95e70bfb60
Anupam Majhi <majhi.anupam@gmail.com>
2023-08-29 05:35:00 +0530

    Quick Fix

---
+++ s3/search_key_bucket.sh
@@ -0,0 +1,9 @@
+﻿
+#!/bin/bash
+## Author: Anupam Majhi
+## Github: https://github.com/anupammajhi/AWS
+
+# Help document function
+help_document() {
+    echo "This script searches for a single keys/object in an S3 bucket and lets you know whether it exists or not."
+    echo "Usage: $0"
