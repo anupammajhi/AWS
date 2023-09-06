2d05a46a34935d2e03e8774e7252d18aac90884f
Anupam Majhi <majhi.anupam@gmail.com>
2023-09-06 06:21:40 +0530

    Commit to change

--- s3/search_multiple_keys_bucket.sh
+++ s3/search_multiple_keys_bucket.sh
@@ -4,3 +4,8 @@
 ## Github: https://github.com/anupammajhi/AWS
 
 if [[ $1 == "help" || $1 == "h" || $1 == "--help" ]]; then
+    echo "Usage: ./search_multiple_keys_bucket.sh"
+    exit
+fi
+
+function check_keys_exist {
