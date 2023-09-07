c27364ea07cb9a96006e204fa52c2da6aede2ad0
Anupam Majhi <majhi.anupam@gmail.com>
2023-09-07 07:30:00 +0530

    Change code

--- s3/search_multiple_keys_bucket.sh
+++ s3/search_multiple_keys_bucket.sh
@@ -11,3 +11,10 @@ fi
 function check_keys_exist {
     bucket="my-bucket"
     keys_to_check=("path/to/file1.txt" "path/to/file2.txt" "path/to/file3.txt")
+
+    existing_keys=$(aws s3 ls s3://$bucket --recursive | awk '{print $4}')
+    
+    for key in "${keys_to_check[@]}"; do
+        if [[ " ${existing_keys[@]} " =~ " $key " ]]; then
+            exists="true"
+        else
