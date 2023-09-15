084f4431ce5455c1005a44e89a7dece776d3e38c
Anupam Majhi <majhi.anupam@gmail.com>
2023-09-15 08:36:36 +0530

    Fix line

--- s3/search_subdirectory.sh
+++ s3/search_subdirectory.sh
@@ -4,3 +4,9 @@ client = boto3.client("s3")
 bucket_name = "bucket_name"
 prefix = ""
 
+result = client.list_objects(Bucket=bucket_name, Delimiter="/")
+
+def ListFiles(client, bucket_name, prefix):
+    _BUCKET_NAME = bucket_name
+    _PREFIX = prefix
+    response = client.list_objects(Bucket=_BUCKET_NAME, Prefix=_PREFIX)
